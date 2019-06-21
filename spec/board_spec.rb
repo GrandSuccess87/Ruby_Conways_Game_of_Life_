b99d306ff6ebac0ce1287cc5a7062968c27bb738
Tessa Anderso <quintessa@8thlight.com>
2019-06-21 14:30:47 -0500

    Add initial failing test

---
+++ spec/board_spec.rb
@@ -0,0 +1,21 @@
+require 'board'
+
+RSpec.describe Board, "find_neighbors" do
+    it "edge square knows its neighbors" do
+        board = Board.new(4)
+
+        neighbors = Board.find_neighbors(board, { x: 0, y: 1 })
+
+        expected_neighbors = [
+            { x: 0, y: 0 },
+            { x: 0, y: 2 },
+            { x: 1, y: 0 },
+            { x: 1, y: 1 },
+            { x: 1, y: 2 },
+            { x: 3, y: 0 },
+            { x: 3, y: 1 },
+            { x: 3, y: 2 },
+        ]
+        expect(neighbors).to eq expected_neighbors
+    end
+end
