492157a52e5a713cd9ed57b29bed54f20de64796
Tessa Anderso <quintessa@8thlight.com>
2019-06-24 17:00:42 -0500

    update test suite for one dimensional world

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,4 +9,20 @@ RSpec.describe Board, "#rows" do
         expect(rows).to eq [[0, 0]]
     end
 
+    it "returns the cells as rows in one dimension" do
+        board = Board.new(4)
+
+        rows = board.rows
+
+        expect(rows).to eq [[0, 0, 0, 0]]
+    end
+
+    it "returns the cells as rows in one dimension" do
+        board = Board.new(10)
+
+        rows = board.rows
+
+        expect(rows).to eq [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    end
+
 end
