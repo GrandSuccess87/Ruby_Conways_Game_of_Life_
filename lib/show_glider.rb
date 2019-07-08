11b3f7fd90ca5a9f29542549216e2b6d06236e96
Tessa Anderso <quintessa@8thlight.com>
2019-07-08 16:05:34 -0500

    Modify code to display the glider

--- lib/show_glider.rb
+++ lib/show_glider.rb
@@ -3,13 +3,13 @@ require_relative 'world'
 require_relative 'world_console'
 
 world = World.new(15)
-location = Location.new(0,0)
+location = Location.new(2,1)
 
-world = World.add_living_cell(world, Location.new(2,1))
-world = World.add_living_cell(world, Location.new(3,2))
-world = World.add_living_cell(world, Location.new(3,3))
-world = World.add_living_cell(world, Location.new(2,3))
-world = World.add_living_cell(world, Location.new(1,3))
+world_with_one_cell = World.add_living_cell(world, location)
+world_with_two_cells = World.add_living_cell(world_with_one_cell, Location.new(3,2))
+world_with_three_cells = World.add_living_cell(world_with_two_cells, Location.new(3,3))
+world_with_four_cells = World.add_living_cell(world_with_three_cells, Location.new(2,3))
+world_with_five_cells = World.add_living_cell(world_with_four_cells, Location.new(1,3))
 
-world_string = WorldConsole.world_to_string(world)
+world_string = WorldConsole.world_to_string(world_with_five_cells)
 puts world_string
